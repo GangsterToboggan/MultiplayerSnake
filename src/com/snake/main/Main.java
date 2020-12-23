725b385b1224d24b803b0cf5efa48bf746d4c757
Zack Porter <zack@zackporter.com>
2020-12-23 16:16:03 -0500

    Merge + Window

--- src/com/snake/main/Main.java
+++ src/com/snake/main/Main.java
@@ -1,10 +1,14 @@
 package com.snake.main;
 
-public class Main {
-	public int Score;
-	
-	
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Main  {
+	public static final int SCREEN_WIDTH = 1200;
+	public static final int SCREEN_HEIGHT = 800;
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		System.out.println("Hello World");
 		System.out.println("ZAck mega gay");
 		Apple Apple = new Apple();
@@ -13,5 +17,11 @@ public class Main {
 		Apple.Test("Nominal");
 		Board.Test("Nominal");
 		Snake.Test("Nominal");
+		JFrame frame = new JFrame("Snake");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
+        frame.add(new SnakeCanvas());
+        frame.setVisible(true);
+
 	}
 }
---
+++ src/com/snake/main/SnakeCanvas.java
@@ -0,0 +1,54 @@
+package com.snake.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+class SnakeCanvas extends JPanel implements MouseListener{
+   
+
+    public SnakeCanvas(){
+        this.addMouseListener(this);
+    }
+
+    public void paintComponent(Graphics g) {
+    	g.setColor(Color.red);
+    	g.drawRect(0, 50, 10, 20);
+    }
+
+    public void mouseClicked(MouseEvent e)
+    {
+        int x = e.getX();
+        int y = e.getY();
+
+    }
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+    // ... other MouseListener methods ... //
+}
