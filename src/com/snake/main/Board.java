659a7c2b96b30972ec29952e895e2b5e15876c1a
Kyle Porter <kyleporter@Kyles-MacBook-Pro.local>
2020-12-23 15:39:52 -0500

    Java1.8

--- src/com/snake/main/Board.java
+++ src/com/snake/main/Board.java
@@ -3,5 +3,6 @@ package com.snake.main;
 public class Board {
 	public static void Test(String S) {
 		System.out.println("Board Class: "+S);
+		
 	}
 }
--- src/com/snake/main/Snake.java
+++ src/com/snake/main/Snake.java
@@ -1,5 +1,7 @@
 package com.snake.main;
 
 public class Snake {
-
+	public static void Test() {
+		//System.o
+	}
 }
