bcae8a726f55354fc8457eae9445e438d799be7a
Kyle Porter <kyleporter@Kyles-MacBook-Pro.local>
2020-12-23 16:44:34 -0500

    added some Snake functions

--- src/com/snake/main/Snake.java
+++ src/com/snake/main/Snake.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
+	public int score;
+	public double snakeWidth;
 	public Vec2 pos;
 	public Vec2 vel;
 	public List<Vec2> tailPositions = new ArrayList<Vec2>();
+	
+	public void createSnake(Vec2 start, Vec2 initialVel) {
+		pos = start;
+		vel = initialVel;
+	}
 	
 	public void Test(String s) {
 		System.out.println("Snake Class: "+s);;
