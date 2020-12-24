9d8e7f65cafdbaab00e8d7ad5fa4f7d8b534e622
Zack Porter <zack@zackporter.com>
2020-12-24 13:09:12 -0500

    Collision and apple stuff

--- src/com/snake/main/Snake.java
+++ src/com/snake/main/Snake.java
@@ -4,18 +4,23 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Snake extends Entity implements Serializable{
-	public int score=50;
-	public double snakeWidth=10;
-	public double speed=50;
+	public static transient final int RESET_SCORE = 10;
+	public static transient final int SNAKE_WIDTH = 10;
+	public static transient final int SNAKE_SPEED = 50;
+	public int score;
+	public double snakeWidth;
+	public double speed;
 	public Vec2 pos;
 	public Vec2 vel;
 	public String username;
 	public List<Vec2> tailPositions = new ArrayList<Vec2>();
 	
 	public  Snake(String username, Vec2 start, Vec2 initialVel) {
+		this.resetSnake();
 		this.username=username;
 		pos = start;
 		vel = initialVel;
@@ -26,7 +31,7 @@ public class Snake extends Entity implements Serializable{
 			g.fillOval((int)vec.x, (int)vec.y, (int)snakeWidth, (int)snakeWidth);
 		}
 	}
-	public void update(double deltaMs) {
+	public void update(double deltaMs, Collection<Snake> snakes, Collection<Apple> apples) {
 		vel.norm();
 		List<Vec2> newTail = new ArrayList<>();
 		
@@ -41,6 +46,38 @@ public class Snake extends Entity implements Serializable{
 		}
 		tailPositions=newTail;
 		
+		
+		/*for (Snake other : snakes) {
+			if (other.isPosOccupied(pos, this.snakeWidth)) {
+				resetSnake();
+			}
+		}*/
+		/*
+		for (Apple apple : apples) {
+			if (this.isPosOccupied(apple.pos,apple.appleWidth)) {
+				apple.setEaten(true);
+				this.score+=apple.numPoints;
+			}
+		}*/
+		
+	}
+	public void resetSnake() {
+		this.pos=new Vec2(300,300);
+		this.vel=new Vec2(5,5);
+		this.score=RESET_SCORE;
+		this.snakeWidth=SNAKE_WIDTH;
+		this.speed=SNAKE_SPEED;
+	}
+	public boolean isPosOccupied(Vec2 pos) {
+		return this.isPosOccupied(pos,0);
+	}
+	public boolean isPosOccupied(Vec2 pos,double additionalRadius) {
+		for (Vec2 tailpos : tailPositions) {
+			if (pos.distTo(tailpos) < this.snakeWidth+additionalRadius) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public void setPos(Vec2 pos) {
