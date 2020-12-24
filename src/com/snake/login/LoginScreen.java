18866936ed2b09ce579f05c50ba646d31a3ad18d
Kyle Porter <kyleporter@Kyles-MacBook-Pro.local>
2020-12-24 18:06:03 -0500

    loggin stuff

--- src/com/snake/login/LoginScreen.java
+++ src/com/snake/login/LoginScreen.java
@@ -52,10 +52,47 @@ public class LoginScreen {
 	      JTextField username = new JTextField();
 	      int usernameWidth = 300;
 	      int usernameHeight = 50;
-	      username.setBounds(3*frameWidth/4-usernameWidth/2,frameWidth/2-usernameHeight/2, usernameWidth, usernameHeight);
+	      username.setBounds(frameWidth/2-usernameWidth/2,7*frameHeight/16-usernameHeight/2, usernameWidth, usernameHeight);
+	      setFontSize(username, 35);
 	      Border border = BorderFactory.createLineBorder(Color.BLACK);
 	      username.setBorder(border);
+	      
+	      JLabel usernameLabel = new JLabel("Username:");
+	      setFontSize(usernameLabel, 40);
+	      Dimension usernameLabelSize = usernameLabel.getPreferredSize();
+	      usernameLabel.setBounds(7*frameWidth/16-usernameLabelSize.width/2,12*frameHeight/32-usernameLabelSize.height/2, usernameLabelSize.width, usernameLabelSize.height);
+	      
+	      
+	      JTextField ip = new JTextField();
+	      int ipWidth = 300;
+	      int ipHeight = 50;
+	      ip.setBounds(frameWidth/2-ipWidth/2,19*frameHeight/32-ipHeight/2, ipWidth, ipHeight);
+	      setFontSize(ip, 35);
+	      ip.setBorder(border);
+	      
+	      JLabel ipLabel = new JLabel("Server Ip Adress:");
+	      setFontSize(ipLabel, 35);
+	      Dimension ipLabelSize = ipLabel.getPreferredSize();
+	      ipLabel.setBounds(frameWidth/2-ipLabelSize.width/2, 17*frameHeight/32-ipLabelSize.height/2, ipLabelSize.width, ipLabelSize.height);
+	      
+	      JButton start = new JButton("Start!");
+	      setFontSize(start,60);
+	      int startWidth = 300;
+	      int startHeight = 100;
+	      start.setBounds(frameWidth/2-startWidth/2, 24*frameHeight/32-startHeight/2, startWidth, startHeight);
+	      start.addActionListener(new ActionListener() { 
+	    	    public void actionPerformed(ActionEvent e) { 
+	    	        connectMethod(username.getText(),ip.getText(),6666);
+	    	        frame.dispose();
+	    	    } 
+	    	});
+	      
+	      
 	      panel.setLayout(null);
+	      panel.add(start);
+	      panel.add(ipLabel);
+	      panel.add(ip);
+	      panel.add(usernameLabel);
 	      panel.add(username);
 	      panel.add(welcome);
 	      panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
@@ -67,11 +104,20 @@ public class LoginScreen {
 		
 	}
 	
-	public static void connectMethod() {
+	public static void connectMethod(String username, String ip, int port) { //ZACK THIS SHOULD HAVE ALL THE INFORMATION YOU NEED
 		System.out.println("lol!");
+		System.out.println(username);
+		System.out.println(ip);
+		System.out.println(""+port);
+	}
+	public static void setFontSize(JTextField field, float size) {
+		 field.setFont (field.getFont ().deriveFont (size));
 	}
 	public static void setFontSize(JLabel label, float size) {
 		 label.setFont (label.getFont ().deriveFont (size));
+	}
+	public static void setFontSize(JButton Button, float size) {
+		 Button.setFont (Button.getFont ().deriveFont (size));
 	}
 }
 
